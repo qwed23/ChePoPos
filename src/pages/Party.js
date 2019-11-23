60c17fcf46e12a2a95b5b7dd80a0771923f1c613
saeed <saeed.dawod@gmail.com>
2019-11-23 03:22:12 +0200

    filter delete

--- src/pages/Party.js
+++ src/pages/Party.js
@@ -4,7 +4,7 @@ import { Link, useParams } from "react-router-dom";
 
 const Party = () => {
   const { status } = useParams();
-  const [state, dispatch] = useReducer(ToolsReducer, {idx: 0});
+  const [state, dispatch] = useReducer(ToolsReducer, { idx: 0 });
   const { idx, split, isDelete } = state;
   const [url, setUrl] = useState();
   const [urlArray, setUrlArray] = useState([
@@ -18,51 +18,49 @@ const Party = () => {
     setUrlArray([...urlArray, url]);
   };
 
-  const deleteHandler = evt => {
-    const tempUrlArray = urlArray.filter(
-      iteam => iteam !== evt.target.dataset.rs
-    );
-    setUrlArray(tempUrlArray);
-  };
-
   const splitFrame = urlArray.map(url => (
     <iframe
-      onClick={isDelete ? deleteHandler : null}
+      onClick={e =>
+        isDelete
+          ? setUrlArray(urlArray.filter(iteam => iteam !== e.target.dataset.rs))
+          : null
+      }
       data-rs={url}
       title={url}
       key={url}
       src={url}
     />
   ));
+
   const oneFrame = <iframe title={urlArray[idx]} src={urlArray[idx]} />;
 
   return (
-      <main>
-        {isDelete && "DELETE MODE ON"}
-        <form onSubmit={submitHandler}>
-          <input
-            value={url}
-            onChange={e => {
-              setUrl(e.target.value);
-            }}
-          />
-          <button type="submit"> Add</button>
-        </form>
+    <main>
+      {isDelete && "DELETE MODE ON"}
+      <form onSubmit={submitHandler}>
+        <input
+          value={url}
+          onChange={e => {
+            setUrl(e.target.value);
+          }}
+        />
+        <button type="submit"> Add</button>
+      </form>
 
-        <aside>
-          {idx + 1}/{urlArray.length}
-          <button onClick={() => {}}> + </button>
-          <button onClick={() => {}}> - </button>
-          <button onClick={() => dispatch({ type: "DELETE" })}> Delete</button>
-          <button onClick={() => dispatch({ type: "NEXT" })}> {"<<"} </button>
-          <button onClick={() => dispatch({ type: "BACK" })}> {">>"} </button>
-          <button onClick={() => dispatch({ type: "SPLIT" })}>
-            <Link to={!split ? `/party/split` : `/party/nosplit`}>Split</Link>
-          </button>
-        </aside>
+      <aside>
+        {idx + 1}/{urlArray.length}
+        <button onClick={() => {}}> + </button>
+        <button onClick={() => {}}> - </button>
+        <button onClick={() => dispatch({ type: "DELETE" })}> Delete</button>
+        <button onClick={() => dispatch({ type: "NEXT" })}> {"<<"} </button>
+        <button onClick={() => dispatch({ type: "BACK" })}> {">>"} </button>
+        <button onClick={() => dispatch({ type: "SPLIT" })}>
+          <Link to={!split ? `/party/split` : `/party/nosplit`}>Split</Link>
+        </button>
+      </aside>
 
-        <section>{status === "split" && split ? splitFrame : oneFrame}</section>
-      </main>
+      <section>{status === "split" && split ? splitFrame : oneFrame}</section>
+    </main>
   );
 };
 
